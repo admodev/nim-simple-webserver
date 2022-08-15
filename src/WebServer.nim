edc6fa32268f1ef9f3cf86ec47748c1061e2fa7a
admodev <admodevcodes@outlook.com>
2022-08-15 12:36:11 -0300

    add domain model

--- src/WebServer.nim
+++ src/WebServer.nim
@@ -1,5 +1,20 @@
 import htmlgen
 import jester
+import norm/[model, sqlite]
+
+# Typedefs for domain model
+type
+  Employee = ref object of Model
+    firstName, lastName: string
+    company: Company
+  Company = ref object of Model
+    name: string
+
+func newCompany(name = ""): Company =
+  Company(name: name)
+
+func newEmployee(firstName = "", lastName = "", company = newCompany()): Employee =
+  Employee(firstName: firstName, lastName: lastName, company: company)
 
 routes:
   get "/":
